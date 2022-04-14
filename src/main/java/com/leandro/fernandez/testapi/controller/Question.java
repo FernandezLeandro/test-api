3385b311eacb72ecfd92a978bd5f0409e714c805
Leandro Fernandez <leanfernandez97@gmail.com>
2022-04-13 21:38:47 -0300

    Creada representacion de una pregunta

---
+++ src/main/java/com/leandro/fernandez/testapi/controller/Question.java
@@ -0,0 +1,9 @@
+package com.leandro.fernandez.testapi.controller;
+
+import java.util.List;
+
+public class Question { //JSON (javascript object notation)
+    public String question;
+    public List<String> posQuestions;
+    public int rta;
+}
