abf145816fdbe48d485958e0328540bb5f278853
Leandro Fernandez <leanfernandez97@gmail.com>
2022-04-16 12:53:00 -0300

    Verifica que la pregunta no se repita, las respuestas sean diferentes, el index sea correcto y que haya min 4 respuestas

--- src/main/java/com/leandro/fernandez/testapi/controller/QuestionController.java
+++ src/main/java/com/leandro/fernandez/testapi/controller/QuestionController.java
@@ -2,6 +2,7 @@ package com.leandro.fernandez.testapi.controller;
 
 import com.leandro.fernandez.testapi.repository.QuestionRepository;
 import lombok.extern.log4j.Log4j2;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,13 +30,73 @@ public class QuestionController {
             return ResponseEntity.ok(questions);
         }
     }
-
+    // Crear question
     @PostMapping
     public ResponseEntity<Question> createQuestion(@RequestBody Question question) {
-        Question createdQuestion = questionRepository.save(question);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdQuestion);
+        // Almaceno las preguntas de questionRepository en una lista
+        ArrayList<Question> questions = new ArrayList<Question>();
+
+        // Por cada objeto encontrado, agregara este a la lista
+        questionRepository.findAll().forEach(questions::add);
+
+        // Verifico si ya existe la pregunta
+        boolean questionIsEqual= false;
+        for (int i=0; i<questions.size(); i++){
+            if (questions.get(i).text.equalsIgnoreCase(question.text)){
+                log.info("Error, the question is repeated");
+                i = questions.size()+1;
+                questionIsEqual = true;
+            }
+        }
+
+        // Si existe, retorno el bad request, sino almaceno correctamente
+        if (questionIsEqual){
+            log.info("Bad Request, question is equal");
+            return ResponseEntity.badRequest().build();
+        } else {
+            // Verifico que haya minimamente 4 respuestas
+            if (question.answers.size() < 4){
+                log.info("Error, there are less than 4 answers");
+                return ResponseEntity.badRequest().build();
+            }
+
+            // Verifica si rightAnswerIndex es valido
+            if (question.rightAnswerIndex < 0 || question.rightAnswerIndex > question.answers.size()-1){
+                log.info("Error, the right answer is incorrect, " + "index is: " + question.rightAnswerIndex);
+                return ResponseEntity.badRequest().build();
+            }
+
+            // Verifica si alguna respuesta es igual
+            boolean answersIsEqual = false;
+            int j;
+            int indexComparativo = 1;
+            for (int i= 0; i<question.answers.size(); i++){
+                j= indexComparativo;
+                while (!answersIsEqual && (j<question.answers.size())){
+                    if (question.answers.get(i).equalsIgnoreCase(question.answers.get(j))){
+                        answersIsEqual = true;
+                        log.info("A answer is equal, salta en " + i + " " + j);
+                    } else j++;
+                }
+                indexComparativo++;
+
+            }
+
+            if (answersIsEqual) {
+                log.info("Bad Request,a asnwser is equal");
+                return ResponseEntity.badRequest().build();
+            } else {
+                Question createdQuestion = questionRepository.save(question);
+                return ResponseEntity.status(HttpStatus.CREATED).body(createdQuestion);
+            }
+        }
+
+
+
+
     }
 
+    // Actualizar question
     @PutMapping("/{id}")
     public ResponseEntity<Question> updateQuestion(@PathVariable Long id, @RequestBody Question questionToUpdate) {
         log.info("Updating question with ID = " + id);
